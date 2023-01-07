be6905850a62d9e821e266931238ad9295c85ef2
Adrian Gallano <adriangallanoengr@gmail.com>
2023-01-07 18:00:02 +0800

    Add game finish

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// all choices
-const choices = document.querySelectorAll("[key-choice]"); 
+
 const compChoices = ["rock", "paper", "scissor"]
-//score
 let playerScore = 0,
 computerScore = 0;
-let roundNumber = 0;
+let roundNumber = 1;
+
+// GUI
+const roundGUI = document.getElementById("round")
+const scoreboard = document.getElementsByClassName("scoreboard")
+const choices = document.querySelectorAll("[key-choice]"); 
+const state = document.getElementById("state")
+const playChoice = document.getElementsByClassName("play-choice")
 
 const getComputerChoice = () => {
     return compChoices[Math.floor(Math.random() * 3)];
@@ -12,47 +17,66 @@ const getComputerChoice = () => {
 
 const round = (playerChoice, computerSelection) => {
     let computerChoice = computerSelection()
-
     roundNumber++;
-
+    displaySelection(playerChoice, computerChoice)
     if(playerChoice === 'rock' && computerChoice === 'scissor' || 
     playerChoice === 'paper' && computerChoice === 'rock' ||
     playerChoice === 'scissor' && computerChoice === 'paper'){
-        
         playerScore++;
-
-        return `Player won ${playerChoice} beats ${computerChoice}`;
+        displayState(`Player won ${playerChoice} beats ${computerChoice}`);
     }else if(playerChoice === computerChoice){
-
-        return `it's a tie!`;
+        displayState(`it's a tie!`);
     }else{
         computerScore++;
-        return `Computer won ${computerChoice} beats ${playerChoice}`
+        displayState(`Computer won ${computerChoice} beats ${playerChoice}`)
     }
 }
 
 const winner = () => {
     if(playerScore > computerScore){
-
         return "playerWon"
     }else if(playerScore < computerScore){
-
         return "computerWon"
-    }else{
-
-        return "it's a tie"
     }
 }
+
+const displayRound = (roundNum) => {
+    roundGUI.textContent = `Round ${roundNum}`
+}
+
+const displayScore = () => {
+    scoreboard[0].textContent = `Player - ${playerScore}`
+    scoreboard[1].textContent = `Computer - ${computerScore}`
+}
+
+const displayState = (status) => {
+    state.textContent = status
+}
+
+const displaySelection = (pChoice, cChoice) => {
+    playChoice[0].src = `./static/images/${pChoice}.png`
+    playChoice[1].src = `./static/images/${cChoice}.png`
+}
+
+const reset = () => {
+    playerScore = 0
+    computerScore = 0
+    roundNumber = 1
+    state.style.visibility = "hidden"
+}
+
 
 const game = () => {
     // initial game state
     Array.from(choices).forEach((choice)=>{
         choice.addEventListener("click", () => {
-
             round(choice.getAttribute("key-choice"), getComputerChoice)
-
+            state.style.visibility = "visible"
+            displayScore()
+            displayRound(roundNumber)
             if(playerScore == 5 || computerScore == 5){
-                console.log(winner()) 
+                winner()
+                reset()
             }
         })
     })
